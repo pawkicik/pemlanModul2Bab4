542cafba356d33e9043fbf098925767bf8ba103a
pawkicik <isawulandari@student.ub.ac.id>
2026-03-24 23:52:29 +0700

    Create MainStudent.java

---
+++ MainStudent.java
@@ -0,0 +1,12 @@
+public class MainStudent {
+    public static void main(String[] args) {
+        // membuat beberapa objek
+        Student s1 = new Student("Anna", "Malang", 20);
+        Student s2 = new Student("Chris", "Kediri", 21);
+        Student s3 = new Student("Sal", "Jakarta", 19, 90, 80, 85);
+
+        s1.jumlahObjek();
+
+        Student s4 = new Student();
+
+        s4.jumlahObjek();
